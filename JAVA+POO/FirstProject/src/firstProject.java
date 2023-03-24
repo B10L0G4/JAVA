89c2f1757841695fe20a283521ccb6c002194109
B1olog4 <vandyyh@gmail.com>
2023-03-24 16:09:15 -0300

    first program ready to test

---
+++ JAVA+POO/FirstProject/src/firstProject.java
@@ -0,0 +1,15 @@
+//public class Main {
+//    public static void main(String[] args) {
+//        System.out.println("Hello world!");
+//    }
+//}
+
+public class firstProject {
+    public void firstMessage(){
+        System.out.println("Olá Mundo!");
+    }
+    public static void main(String[] args) {
+        firstProject ExProject = new firstProject();
+        ExProject.firstMessage();
+    }
+}
