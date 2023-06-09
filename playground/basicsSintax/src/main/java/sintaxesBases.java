cc7d069335e4f5943dbfacbf7f521fe8d984017e
B10l0g4 <vandyyh@gmail.com>
2023-06-09 12:35:09 -0300

    sintaxes bases

---
+++ playground/basicsSintax/src/main/java/sintaxesBases.java
@@ -0,0 +1,15 @@
+public class sintaxesBases {
+    public static void main(String[] args) {
+        String myName = "Ane";
+
+        int age = 38 - 10;
+        boolean isReal = true;
+
+
+        if (age == 38 )
+        System.out.print(isReal);
+        else {
+            System.out.print(!isReal);
+        }
+    }
+}
