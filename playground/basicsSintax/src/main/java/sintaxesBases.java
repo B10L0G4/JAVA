80a7827098e423a25014cd74a1bdbbbe646ff5b0
B10l0g4 <vandyyh@gmail.com>
2023-06-20 09:55:54 -0300

    exemplos

--- playground/basicsSintax/src/main/java/sintaxesBases.java
+++ playground/basicsSintax/src/main/java/sintaxesBases.java
@@ -2,6 +2,11 @@ public class sintaxesBases {
     public static void main(String[] args) {
         String myName = "Ane";
 
+        int numero = -5;
+        numero = - numero;
+
+        System.out.println(numero);
+
         int age = 38 - 10;
         boolean isReal = true;
 
