49292090ad9900e4331c4e90ea3bdb59f23dba9a
B10l0g4 <vandyyh@gmail.com>
2023-06-22 18:02:24 -0300

    adicionando excepiton e condicionais

--- DesafioControleFluxo/src/main/java/Contador.java
+++ DesafioControleFluxo/src/main/java/Contador.java
@@ -1,3 +1,37 @@
+import ParametrosInvalidosException.ParametrosInvalidosException;
+
+import java.util.Scanner;
+
 public class Contador {
+
+        public static void main(String[] args) {
+            Scanner terminal = new Scanner(System.in);
+            System.out.println("Digite o primeiro parâmetro");
+            int parametroUm = terminal.nextInt();
+            System.out.println("Digite o segundo parâmetro");
+            int parametroDois = terminal.nextInt();
+
+            try {
+                //chamando o método contendo a lógica de contagem
+                contar(parametroUm, parametroDois);
+
+            } catch ( ParametrosInvalidosException e) {
+                new ParametrosInvalidosException();
+                //imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+            }
+
+        }
+        static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+            if (parametroUm == parametroDois) {
+                System.out.println("Parâmetros inválidos");
+            }
+
+
+            int contagem = parametroDois - parametroUm;
+            for (int i = 0; i <= contagem; i++) {
+                System.out.println(parametroUm + i);
+            //realizar o for para imprimir os números com base na variável contagem
+        }
+    }
 
 }
