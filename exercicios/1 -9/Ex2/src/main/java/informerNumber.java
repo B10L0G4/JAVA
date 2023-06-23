85139ef3ab3b306a51d98c8509c204c7ed8d01c3
B10l0g4 <vandyyh@gmail.com>
2023-06-23 08:50:47 -0300

    ex2 - lista de numeros

--- exercicios/1 -9/Ex2/src/main/java/informerNumber.java
+++ exercicios/1 -9/Ex2/src/main/java/informerNumber.java
@@ -1,2 +1,13 @@
+// 2- Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+
+import java.util.Scanner;
+
 public class informerNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); // system.in faz a leitura do teclado
+        System.out.println("Digite um número:");
+        int number = scanner.nextInt(); // nextInt() faz a leitura do número inteiro
+
+        System.out.println("O número informado foi:" + number);
+    }
 }
