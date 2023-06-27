1d9bd3668f384e66da6de7952579fbb965678227
B10l0g4 <vandyyh@gmail.com>
2023-06-27 07:44:53 -0300

    exemplo de ususo de constructor, dicas e implementação

--- playground/GatterAndSetters/src/main/java/ModeloConstructor/CadastroDeCliente.java
+++ playground/GatterAndSetters/src/main/java/ModeloConstructor/CadastroDeCliente.java
@@ -1,4 +1,19 @@
 package ModeloConstructor;
 
 public class CadastroDeCliente {
+    public static void main(String[] args) {
+        ModeloDeCadastro cadastro = new ModeloDeCadastro("Vanessa", 123456789.0, "Rua das borboletas, 0");
+        // os parametros no construtor são os mesmos que foram definidos na classe ModeloDeCadastro
+        //devem ser passados na mesma ordem e tipo , além de serem separados por virgula.
+        cadastro.setNome("Ane");
+        cadastro.setCpf(122456789.0);
+        cadastro.setEndereco("Rua dos bobos, 0");
+
+        System.out.println(cadastro.getNome() + "CPF:  " + cadastro.getCpf() + " Endereço: " + cadastro.getEndereco());
+
+        // quando usar um construtor?
+        // quando você quer que o objeto seja criado com os parametros
+        // posso criar construtores sem parametros, com fields e com todos os parametros
+        // boas práticas dizem que devemos criar um construtor sem parametros e um com todos os parametros
+    }
 }
--- playground/GatterAndSetters/src/main/java/ModeloConstructor/ModeloDeCadastro.java
+++ playground/GatterAndSetters/src/main/java/ModeloConstructor/ModeloDeCadastro.java
@@ -5,6 +5,16 @@ public class ModeloDeCadastro {
     private Double cpf;
     private String endereco;
 
+    public ModeloDeCadastro(String nome, Double cpf, String endereco) {
+        // O construtor é responsavél por criar um objeto com os parametros passados
+        // O construtor deve ter o mesmo nome da classe e não deve ter retorno
+        // o this é responsavél por referenciar a variavel da classe, ou seja é um atributo da
+        // classe , diferentemente do parametro que é uma variavel local
+        this.nome = nome;
+        this.cpf = cpf;
+        this.endereco = endereco;
+    }
+
     public String getNome() {
         return nome;
     }
