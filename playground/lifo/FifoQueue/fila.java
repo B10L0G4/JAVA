214c37174280c96d65aedf4d03ba9fc1ecaad064
B10l0g4 <vandyyh@gmail.com>
2023-07-11 11:31:29 -0300

    adicionando implementação da classe fila

--- playground/lifo/FifoQueue/fila.java
+++ playground/lifo/FifoQueue/fila.java
@@ -5,16 +5,61 @@ public class fila {
 
     public fila() {
         this.refNoDaFilaEntrada = null;
-    }
+    } // definie o nó de entrada como nulo
 
-    public void enqueue(noDaFila novoNo){
-        novoNo.setRefNoDaFila(refNoDaFilaEntrada);
-        refNoDaFilaEntrada = novoNo;
+    public void enqueue(noDaFila novoNo){ // Emfileirar
+        novoNo.setRefNoDaFila(refNoDaFilaEntrada); // O novo nó aponta para o nó que está na entrada da fila
+        refNoDaFilaEntrada = novoNo; // O nó que está na entrada da fila passa a ser o novo nó
     }
-    public void first(){
-
+    public noDaFila first(){ // retorna o primeiro nó da fila
+        if (!this.isEmpty()){
+            noDaFila primeiroNo = refNoDaFilaEntrada; // Cria um nó auxiliar que recebe o nó que está na entrada da fila
+            while(true){//faz um loop infinito até chegar ao primeiro da fila
+                if(primeiroNo.getRefNoDaFila() != null){//verifica se o nó auxiliar não é nulo
+                    primeiroNo = primeiroNo.getRefNoDaFila();//se não for nulo, o nó auxiliar recebe o próximo nó
+                }
+            }
+        }
+        return null;
+    }
+    public noDaFila dequeue (){
+        if (!this.isEmpty()){
+            noDaFila primeiroNo = refNoDaFilaEntrada; // primeiroNo recebe o nó que está na entrada da fila
+            noDaFila noAuxiliar = refNoDaFilaEntrada; // Cria um nó auxiliar que recebe o nó que está na entrada da fila
+            while(true){//faz um loop infinito até chegar ao primeiro da fila
+                if(primeiroNo.getRefNoDaFila() != null){//verifica se o nó auxiliar não é nulo
+                    noAuxiliar = primeiroNo; // o nó auxiliar recebe o nó que está na entrada da fila
+                    primeiroNo = primeiroNo.getRefNoDaFila();//se não for nulo, o nó auxiliar recebe o próximo nó
+                }else{
+                    noAuxiliar.setRefNoDaFila(null); // o nó auxiliar aponta para nulo
+                    break; // sai do loop
+                }
+            }
+            return primeiroNo;
+        }
+        return null;
     }
     public boolean isEmpty(){
         return refNoDaFilaEntrada == null ? true : false;
+    } // Verifica se a fila está vazia
+
+    @Override
+    public String toString() {
+        String stringRetorno = "";//cria uma string vazia
+        noDaFila noAuxiliar = refNoDaFilaEntrada; // cria um nó auxiliar que recebe o nó que está na entrada da fila
+        if (refNoDaFilaEntrada != null){ // verifica se o nó que está na entrada da fila não é nulo
+            while(true){
+                stringRetorno += "[No{objeto=" + noAuxiliar.getObject() + "}]--->"; // adiciona na string o objeto que está no nó auxiliar
+                if (noAuxiliar.getRefNoDaFila() != null){ // verifica se o nó auxiliar não é nulo
+                    noAuxiliar = noAuxiliar.getRefNoDaFila(); // o nó auxiliar recebe o próximo nó
+                }else{
+                    stringRetorno += "null"; // se o nó auxiliar for nulo, a string recebe null
+                    break; // sai do loop quando o nó auxiliar for nulo
+                }
+            }
+        }else{
+            stringRetorno = "null"; // se o nó que está na entrada da fila for nulo, a string recebe null
+        }
+        return stringRetorno; // retorna a string
     }
 }
--- playground/lifo/FifoQueue/noDaFila.java
+++ playground/lifo/FifoQueue/noDaFila.java
@@ -29,4 +29,6 @@ public class noDaFila {
     }
 
 
+    public String getObject() {
+    }
 }
