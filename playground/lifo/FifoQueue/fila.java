a0516d8cfbd10dc4b0e77e2abfc8080f7761cde8
B10l0g4 <vandyyh@gmail.com>
2023-07-10 23:28:10 -0300

    constructor, getters and setters , para fila

--- playground/lifo/FifoQueue/fila.java
+++ playground/lifo/FifoQueue/fila.java
@@ -1,5 +1,20 @@
 package FifoQueue;
 
 public class fila {
+    private noDaFila refNoDaFilaEntrada;
 
+    public fila() {
+        this.refNoDaFilaEntrada = null;
+    }
+
+    public void enqueue(noDaFila novoNo){
+        novoNo.setRefNoDaFila(refNoDaFilaEntrada);
+        refNoDaFilaEntrada = novoNo;
+    }
+    public void first(){
+
+    }
+    public boolean isEmpty(){
+        return refNoDaFilaEntrada == null ? true : false;
+    }
 }
