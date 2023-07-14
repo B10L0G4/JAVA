35ab07d5259f758c5bcf621d89d6c3fa6585a1ce
B10l0g4 <vandyyh@gmail.com>
2023-07-14 08:59:55 -0300

    implementação do get e getNo

--- playground/Listas/src/main/java/noListaEncadeada/ListaEncadeada.java
+++ playground/Listas/src/main/java/noListaEncadeada/ListaEncadeada.java
@@ -6,6 +6,35 @@ public class ListaEncadeada<T> {
         this.refEntrada = null;
     }
 
+    public void add(T conteudo){
+        noDaLista<T> novoNo = new noDaLista<>(conteudo);
+        if(this.isEmpty()){
+            refEntrada = novoNo;
+            return;
+        }
+        noDaLista<T> noAuxiliar = refEntrada;
+        for(int i = 0; i < this.size()-1; i++){
+            noAuxiliar = noAuxiliar.getProximoNo();
+        }
+        noAuxiliar.setProximoNo(novoNo);
+    }
+
+    public T get(int index){
+        return getNo(index).getConteudo();
+    }
+
+    private noDaLista<T> getNo(int index){
+        validaIndice(index);
+        noDaLista<T> noAuxiliar = refEntrada;
+        noDaLista<T> noRetorno = null;
+
+        for(int i = 0; i <= index; i++){
+            noRetorno = noAuxiliar;
+            noAuxiliar = noAuxiliar.getProximoNo();
+        }
+        return noRetorno;
+    }
+
     public int size(){
         int tamanhoLista = 0;
         noDaLista<T> refAuxiliar = refEntrada;
@@ -22,6 +51,15 @@ public class ListaEncadeada<T> {
             }
         }
         return tamanhoLista;
+    }
+
+    private void validaIndice(int index){
+        if(index >= size()){
+            int ultimoIndice = size() - 1;
+            throw new IndexOutOfBoundsException("Não existe conteúdo no índice "
+                    + index + " desta lista. Esta lista só vai até o índice "
+                    + ultimoIndice + ".");
+        }
     }
     public boolean isEmpty(){
         return refEntrada == null ? true : false;
