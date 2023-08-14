1b421aabbfeeb25275d40a94b2ac3b29d83eaa60
B10l0g4 <vandyyh@gmail.com>
2023-08-14 19:24:42 -0300

    100 exercicios em Java

---
+++ ListsExercicios/src/main/java/listExemples/ListNotes.java
@@ -0,0 +1,14 @@
+package listExemples;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListNotes {
+    public static void main(String[] args) {
+        // List notas = new ArrayList(); // list sem generics (sem especificar o tipo de dado que a lista vai receber)
+        // List<Double> notas = new ArrayList<>();// list com generics (especificando o tipo de dado que a lista vai receber)
+
+        // generics e diamond operator : São usados para especificar o tipo de dado que a lista vai receber
+
+    }
+}
